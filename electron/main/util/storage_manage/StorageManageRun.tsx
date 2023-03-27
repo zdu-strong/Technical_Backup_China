c9089ca5b9b61e197c0538a5a5ac0004d0f9478d
zdu <zdu.strong@gmail.com>
2023-03-27 20:32:35 +0800

    #: Simplify the code

--- electron/main/util/storage_manage/StorageManageRun.tsx
+++ electron/main/util/storage_manage/StorageManageRun.tsx
@@ -1,24 +1,31 @@
 import { StorageSpaceService } from "@/service";
-import { concat, interval, concatMap, lastValueFrom, of, catchError } from "rxjs";
+import { concat, interval, concatMap, lastValueFrom, of, retry } from "rxjs";
 import { from } from "linq";
 import { listRoots } from "@/util/StorageUtil";
 
 async function runManageStorageSpace() {
-  const totalPage = (
-    await StorageSpaceService.getStorageSpaceListByPagination(1, 1)
-  ).totalPage;
+  const { totalPage } = await StorageSpaceService.getStorageSpaceListByPagination(1, 1);
   for (let i = totalPage; i > 0; i--) {
-    const result = await StorageSpaceService.getStorageSpaceListByPagination(i, 1);
-    for (const storageSpaceModel of result.list) {
-      if (!(await StorageSpaceService.isUsed(storageSpaceModel.folderName))) {
-        await StorageSpaceService.deleteFolder(storageSpaceModel.folderName);
+    try {
+      const result = await StorageSpaceService.getStorageSpaceListByPagination(i, 1);
+      for (const storageSpaceModel of result.list) {
+        if (!(await StorageSpaceService.isUsed(storageSpaceModel.folderName))) {
+          await StorageSpaceService.deleteFolder(storageSpaceModel.folderName);
+        }
       }
+    } catch (error) {
+      // do nothing
     }
   }
+
   const folderNameListOfRootFolder = await listRoots();
   for (const folderName of folderNameListOfRootFolder) {
-    if (!(await StorageSpaceService.isUsed(folderName))) {
-      await StorageSpaceService.deleteFolder(folderName);
+    try {
+      if (!(await StorageSpaceService.isUsed(folderName))) {
+        await StorageSpaceService.deleteFolder(folderName);
+      }
+    } catch (error) {
+      // do nothing
     }
   }
 }
@@ -29,7 +36,7 @@ async function main() {
       concatMap(() => {
         return from(runManageStorageSpace());
       }),
-      catchError((error, caught) => caught),
+      retry(),
     )
   );
 }
