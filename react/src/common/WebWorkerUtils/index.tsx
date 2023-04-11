611906046cd0966570c85c0f2951ff1e488e5def
zdu <zdu.strong@gmail.com>
2023-04-11 12:58:38 +0800

    #: Optimize code

--- react/src/common/WebWorkerUtils/index.tsx
+++ react/src/common/WebWorkerUtils/index.tsx
@@ -3,8 +3,8 @@ import WebworkerPromise from 'webworker-promise'
 
 export async function runWoker(worker: Worker, params?: any) {
   const workerSubject = new ReplaySubject();
-  worker.onerror = () => {
+  worker.addEventListener("error", () => {
     workerSubject.error(new Error("Webworker file load failed!"));
-  };
+  });
   return await Promise.race([new WebworkerPromise(worker).postMessage(params || {}), workerSubject.toPromise()]);
 }
