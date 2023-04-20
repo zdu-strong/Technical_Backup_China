47f60ec94b03f409e8f9b73f5a3beb9483843d67
zdu <zdu.strong@gmail.com>
2023-04-20 13:30:14 +0800

    #: Modify the way to generate random number

--- electron/src/component/HomePageComponent/js/useRandomNumber.tsx
+++ electron/src/component/HomePageComponent/js/useRandomNumber.tsx
@@ -1,6 +1,6 @@
 import { useMobxState } from "mobx-react-use-autorun";
 import { useMount, useUnmount } from "mobx-react-use-autorun"
-import { concatMap, EMPTY, interval, of, repeat, Subscription, take } from "rxjs";
+import { concatMap, EMPTY, interval, of, repeat, Subscription, take, tap } from "rxjs";
 
 export function useRandomNumber() {
   const state = useMobxState({
@@ -14,14 +14,16 @@ export function useRandomNumber() {
         concatMap(() => {
           const randomNumber = Math.floor(Math.random() * 100 + 1);
           if (state.randomNumber !== randomNumber) {
-            state.randomNumber = randomNumber;
-            return of(null);
+            return of(randomNumber);
           } else {
             return EMPTY;
           }
         }),
         take(1),
       )),
+      tap((randomNumber) => {
+        state.randomNumber = randomNumber;
+      }),
       repeat({ delay: 1000 }),
     ).subscribe());
   }
