693f0bd07f7c2b08f03e088bee4722099ee47c06
zdu <zdu.strong@gmail.com>
2023-06-03 20:41:33 +0800

    #: Update project config of react

--- react/config-overrides.js
+++ react/config-overrides.js
@@ -1,10 +1,12 @@
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 const path = require('path');
+const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 
 module.exports = function override(config) {
   config.plugins = (config.plugins || []).concat([
     new NodePolyfillPlugin()
   ]);
   config.resolve.alias['@'] = path.join(__dirname, 'src');
+  config.resolve.plugins = config.resolve.plugins.filter(plugin => !(plugin instanceof ModuleScopePlugin));
   return config;
 };
