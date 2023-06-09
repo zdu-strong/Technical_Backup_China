c79d298860f2f9936b4de333b62dcd56696f5556
zdu <zdu.strong@gmail.com>
2023-06-09 11:47:28 +0800

    #: Optimize code

--- react/src/component/MessageEntry/MessageEntry.tsx
+++ react/src/component/MessageEntry/MessageEntry.tsx
@@ -19,6 +19,7 @@ export default observer(() => {
       state.readyForMessageList = readyForMessageList;
     },
     error: null as any,
+    keyOfDiv: v1(),
     css: stylesheet({
       container: {
         width: "100%",
@@ -41,6 +42,7 @@ export default observer(() => {
       if (!(await api.Authorization.isSignIn())) {
         const { data: newAccount } = await api.Authorization.createNewAccountOfSignUp();
         await api.Authorization.signUp(newAccount.id, v1(), "visitor", [], newAccount.publicKeyOfRSA);
+        state.keyOfDiv = v1();
       }
       state.readyForStart = true;
     } catch (error) {
@@ -50,10 +52,10 @@ export default observer(() => {
 
   return <>
     <LoadingOrErrorComponent ready={state.readyForStart && state.readyForMessageList} error={state.error} />
-    {state.readyForStart && <div className={state.css.container} style={state.readyForMessageList ? {} : { position: "absolute", visibility: "hidden" }} >
+    <div key={state.keyOfDiv} className={state.css.container} style={state.readyForStart && state.readyForMessageList ? {} : { position: "absolute", visibility: "hidden" }} >
       <MessageMenu userId={GlobalUserInfo.id} username={GlobalUserInfo.username} />
       <MessageUnlimitedList userId={GlobalUserInfo.id!} username={GlobalUserInfo.username!} setReadyForMessageList={state.setReadyForMessageList} />
       <MessageChat userId={GlobalUserInfo.id!} username={GlobalUserInfo.username!} />
-    </div>}
+    </div>
   </>
 })
