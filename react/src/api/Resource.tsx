cd7e289fab308aadf73057f21435e40945f5528e
zdu <zdu.strong@gmail.com>
2023-06-14 06:52:55 +0800

    #: Get upload speed

--- react/src/api/Resource.tsx
+++ react/src/api/Resource.tsx
@@ -1,15 +1,34 @@
-import { timer } from "rxjs";
+import { Subject, timer } from "rxjs";
 import { ServerAddress } from "@/common/Server";
 import { runWoker } from '@/common/WebWorkerUtils';
 
-export async function upload(file: File): Promise<{ url: string, downloadUrl: string }> {
+export async function upload(
+  file: File,
+  uploadProgressSubject?: UploadProgressSubjectType,
+): Promise<{ url: string, downloadUrl: string }> {
   for (let i = 10; i > 0; i--) {
     await timer(1).toPromise();
   }
-  return await runWoker(new Worker(new URL('../common/WebWorker/UploadResource/UploadResource.worker', import.meta.url), { type: "module" }),
+  const worker = new Worker(new URL('../common/WebWorker/UploadResource/UploadResource.worker', import.meta.url), { type: "module" });
+  if (uploadProgressSubject) {
+    worker.addEventListener("message", (e) => {
+      if (e.data[2] !== "onUploadProgress") {
+        return;
+      }
+      uploadProgressSubject.next(e.data[3]);
+    });
+  }
+  return await runWoker(worker,
     {
       ServerAddress,
       file: file
     }
   );
 }
+
+export type UploadProgressSubjectType = Subject<{
+  total: number,
+  loaded: number,
+  // unit is B/second. It is the average speed when the upload is complete.
+  speed: number,
+}>;
--- react/src/common/WebWorker/UploadResource/UploadResource.worker.tsx
+++ react/src/common/WebWorker/UploadResource/UploadResource.worker.tsx
@@ -4,6 +4,8 @@ import axios from "axios";
 import { catchError, concatMap, from, map, of, range, toArray } from "rxjs";
 import * as mathjs from 'mathjs'
 import { getLongTermTask } from '@/api/LongTermTask';
+import { addMilliseconds } from 'date-fns'
+import linq from 'linq'
 
 registerWebworker(async ({
   ServerAddress,
@@ -11,17 +13,64 @@ registerWebworker(async ({
 }: {
   ServerAddress: string
   file: File
-}) => {
+}, emit) => {
   if (!ServerAddress) {
     throw new Error("Server Address cannot be empty");
   }
   /* Each piece is 10MB */
   const everySize = 1024 * 1024 * 10;
+  // unit is milliseconds
+  const durationOfCalculateSpeed = 1000;
+  const uplodStartDate = new Date();
+  type UploadProgessType = {
+    createDate: Date,
+    loaded: number,
+    total: number,
+  };
+  let uploadProgressList: UploadProgessType[] = [{
+    createDate: new Date(),
+    loaded: 0,
+    total: file.size,
+  }];
   const url: String | undefined = await range(1, mathjs.max(mathjs.ceil(mathjs.divide(file.size, everySize)), 1)).pipe(
     concatMap((pageNum) => {
       const formData = new FormData();
       formData.set("file", new File([file.slice((pageNum - 1) * everySize, pageNum * everySize)], file.name, file));
-      return from(axios.post(`${ServerAddress}/upload/resource`, formData));
+      return from(axios.post(`${ServerAddress}/upload/resource`, formData, {
+        onUploadProgress(progressEvent) {
+          const loaded = (pageNum - 1) * everySize + Math.floor(mathjs.divide(mathjs.multiply((formData.get("file") as File).size, progressEvent.loaded), progressEvent.total!));
+          const total = file.size;
+          const nowDate = new Date();
+          const beforeDate = addMilliseconds(nowDate, 0 - durationOfCalculateSpeed);
+          let uploadProgess: UploadProgessType = linq.from(uploadProgressList).where(s => s.createDate.getTime() <= beforeDate.getTime()).orderByDescending(s => s.createDate).firstOrDefault()!;
+          if (!uploadProgess) {
+            uploadProgess = linq.from(uploadProgressList).orderBy(s => s.createDate).first();
+          } else {
+            while (true) {
+              const index = uploadProgressList.findIndex(s => s.createDate.getTime() < uploadProgess.createDate.getTime());
+              if (index < 0) {
+                break;
+              }
+              uploadProgressList.splice(0, 1);
+            }
+          }
+          // unit is B/second
+          let speed = Math.floor(mathjs.divide(loaded - uploadProgess.loaded, mathjs.divide(nowDate.getTime() - uploadProgess.createDate.getTime(), 1000)));
+          if (loaded === total) {
+            speed = Math.floor(mathjs.divide(total, mathjs.divide(nowDate.getTime() - uplodStartDate.getTime(), 1000)));
+          }
+          uploadProgressList.push({
+            createDate: nowDate,
+            loaded,
+            total,
+          });
+          emit("onUploadProgress", {
+            total,
+            loaded,
+            speed,
+          });
+        }
+      }));
     }),
     map((response) => response.data),
     toArray(),
