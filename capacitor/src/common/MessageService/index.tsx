6564201454e07d3394ce49f4b521c2d2922707cb
zdu <zdu.strong@gmail.com>
2023-06-17 22:13:51 +0800

    #: Optimize code

--- capacitor/src/common/MessageService/index.tsx
+++ capacitor/src/common/MessageService/index.tsx
@@ -1,56 +1,61 @@
 import { v1 } from 'uuid';
 import { observable } from 'mobx-react-use-autorun'
 import { FormattedMessage } from 'react-intl';
-import { GlobalExactMessageMatch } from '@/common/MessageService/GlobalExactMessageMatch';
+import { GlobalExactMessageMatch } from '@/common/MessageMatch/GlobalExactMessageMatch'
+import { ReactNode } from 'react';
+import { getFuzzyMessageMatch } from '@/common/MessageMatch/GlobalFuzzyMessageMatch';
 
 export const MessageService = {
-  error: (message: string | string[] & any) => {
+  error: (message: string | string[] | Error | Error[] | any) => {
     handleMessage(MESSAGE_TYPE_ENUM.error, message)
   },
   warning: (message: string | string[] | any) => {
     handleMessage(MESSAGE_TYPE_ENUM.warning, message)
   },
-  info: (message: string | string[] & any) => {
+  info: (message: string | string[] | any) => {
     handleMessage(MESSAGE_TYPE_ENUM.info, message)
   },
-  success: (message: string | string[] & any) => {
+  success: (message: string | string[] | any) => {
     handleMessage(MESSAGE_TYPE_ENUM.success, message)
   }
 }
 
-export const GlobalMessageList = observable([]) as { id: string, message: string, type: string }[];
+export const GlobalMessageList = observable([]) as { id: string, message: ReactNode, type: string }[];
 
-export function getI18nMessageReactNode(message: string) {
+export const MESSAGE_TYPE_ENUM = {
+  error: "error" as "error",
+  warning: "warning" as "warning",
+  info: "info" as "info",
+  success: "success" as "success",
+}
+
+export function getMessageObject(type: "error" | "warning" | "info" | "success", message: any) {
+  const messageString = getMessageString(message);
+  const messageOfI18n = getI18nMessageReactNode(messageString);
+  return {
+    id: v1(),
+    message: messageOfI18n,
+    type: type
+  }
+}
+
+function getI18nMessageReactNode(message: string) {
   let messageOfI18n = getMessageString(message);
   if (GlobalExactMessageMatch[message]) {
     return <FormattedMessage id={GlobalExactMessageMatch[message]} defaultMessage={message} />
+  } else {
+    return getFuzzyMessageMatch(messageOfI18n);
   }
-  return messageOfI18n;
 }
 
-export const MESSAGE_TYPE_ENUM = {
-  error: "error",
-  warning: "warning",
-  info: "info",
-  success: "success"
-}
-
-function handleMessage(type: string, message: string | string[]) {
+function handleMessage(type: "error" | "warning" | "info" | "success", message: string | string[] | Error | Error[] | any) {
   GlobalMessageList.splice(0, GlobalMessageList.length);
   if (message instanceof Array) {
     for (const messageItem of message) {
-      GlobalMessageList.push(getMessageObject(type, messageItem))
+      handleMessage(type, messageItem)
     }
   } else {
     GlobalMessageList.push(getMessageObject(type, message))
-  }
-}
-
-function getMessageObject(type: string, message: any) {
-  return {
-    id: v1(),
-    message: getMessageString(message),
-    type: type
   }
 }
 
--- electron/src/common/MessageService/GlobalMessageComponent.tsx
+++ electron/src/common/MessageService/GlobalMessageComponent.tsx
@@ -1,5 +1,5 @@
 import { Alert, Dialog, DialogContent, DialogTitle, Divider, Fab } from "@mui/material"
-import { GlobalMessageList, MessageService, MESSAGE_TYPE_ENUM, getI18nMessageReactNode } from "."
+import { GlobalMessageList, MessageService, MESSAGE_TYPE_ENUM } from "@/common/MessageService"
 import linq from "linq";
 import { useMobxState, observer } from 'mobx-react-use-autorun';
 import CloseIcon from '@mui/icons-material/Close';
@@ -33,14 +33,14 @@ export default observer(() => {
     if (GlobalMessageList.length > 0) {
       const type = linq.from(GlobalMessageList).select(s => s.type).first();
       if (type === MESSAGE_TYPE_ENUM.warning) {
-        return 'warning';
+        return MESSAGE_TYPE_ENUM.warning;
       } else if (type === MESSAGE_TYPE_ENUM.info) {
-        return 'info';
+        return MESSAGE_TYPE_ENUM.info;
       } else if (type === MESSAGE_TYPE_ENUM.success) {
-        return 'success';
+        return MESSAGE_TYPE_ENUM.success;
       }
     }
-    return "error";
+    return MESSAGE_TYPE_ENUM.error;
   }
 
   function getMessageContentTextColor() {
@@ -114,8 +114,8 @@ export default observer(() => {
       <Divider />
       <DialogContent style={{ padding: "1em" }}>
         {GlobalMessageList.map(messsageObject =>
-          <Alert severity={messsageObject.type as any} className={state.css.alertMessageContent} key={messsageObject.id} style={{ marginTop: "1em", color: getMessageContentTextColor() }}>
-            {getI18nMessageReactNode(messsageObject.message)}
+          <Alert severity={messsageObject.type as any} className={state.css.alertMessageContent} key={messsageObject.id} style={{ marginTop: "1em", color: getMessageContentTextColor(), whiteSpace: "pre-wrap", wordBreak: "break-word", wordWrap: "break-word" }}>
+            {messsageObject.message}
           </Alert>
         )}
       </DialogContent>
