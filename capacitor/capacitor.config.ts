422af26dc89260f0aa3029e56a44ba59b86d084e
zdu <zdu.strong@gmail.com>
2023-07-04 14:25:13 +0800

    #: Update versions of dependencies

--- capacitor/capacitor.config.ts
+++ capacitor/capacitor.config.ts
@@ -14,6 +14,7 @@ const config: CapacitorConfig =
       keystorePassword: "123456",
       keystoreAlias: "android",
       keystoreAliasPassword: "123456",
+      signingType: "apksigner",
     }
   },
 };
