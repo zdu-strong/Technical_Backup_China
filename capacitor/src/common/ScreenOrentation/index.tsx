e2383ddf45e1e0fc7e8df046bf8ce6efc90ccab3
zdu <zdu.strong@gmail.com>
2023-07-15 10:41:50 +0800

    #: Fix bug of the screen does not rotate correctly

--- capacitor/src/common/ScreenOrentation/index.tsx
+++ capacitor/src/common/ScreenOrentation/index.tsx
@@ -2,39 +2,49 @@ import { StatusBar } from '@capacitor/status-bar'
 import { ScreenOrientation } from '@awesome-cordova-plugins/screen-orientation'
 import { Capacitor } from '@capacitor/core'
 import { AndroidFullScreen } from '@awesome-cordova-plugins/android-full-screen'
+import { ReplaySubject, concatMap, from, retry } from 'rxjs'
+
+const subject = new ReplaySubject<"LANDSCAPE" | "PORTRAIT_PRIMARY">(1);
+
+subject.next("PORTRAIT_PRIMARY");
+
+subject.pipe(
+  concatMap((type) => from((async () => {
+    if (type === "LANDSCAPE") {
+      if (Capacitor.getPlatform() === "web") {
+        return;
+      }
+
+      await ScreenOrientation.lock(ScreenOrientation.ORIENTATIONS.LANDSCAPE);
+
+      if (Capacitor.getPlatform() === "android") {
+        await AndroidFullScreen.immersiveMode()
+      }
+
+      await StatusBar.hide();
+      await StatusBar.setOverlaysWebView({ overlay: true });
+    } else if (type === "PORTRAIT_PRIMARY") {
+      if (Capacitor.getPlatform() === "web") {
+        return;
+      }
+
+      await ScreenOrientation.lock(ScreenOrientation.ORIENTATIONS.PORTRAIT_PRIMARY);
+      await StatusBar.setOverlaysWebView({ overlay: false });
+
+      if (Capacitor.getPlatform() === "android") {
+        await AndroidFullScreen.showSystemUI()
+      }
+
+      await StatusBar.show();
+    }
+  })())),
+  retry(),
+).subscribe();
 
 export async function LANDSCAPE() {
-  if (Capacitor.getPlatform() === "web") {
-    return;
-  }
-
-  await ScreenOrientation.lock(ScreenOrientation.ORIENTATIONS.LANDSCAPE);
-
-  if (Capacitor.getPlatform() === "android") {
-    await AndroidFullScreen.immersiveMode()
-  }
-
-  await StatusBar.hide();
-  await StatusBar.setOverlaysWebView({ overlay: true });
+  subject.next("LANDSCAPE");
 }
 
 export async function PORTRAIT_PRIMARY() {
-  if (Capacitor.getPlatform() === "web") {
-    return;
-  }
-
-  await ScreenOrientation.lock(ScreenOrientation.ORIENTATIONS.PORTRAIT_PRIMARY);
-  await StatusBar.setOverlaysWebView({ overlay: false });
-
-  if (Capacitor.getPlatform() === "android") {
-    await AndroidFullScreen.showSystemUI()
-  }
-
-  await StatusBar.show();
+  subject.next("PORTRAIT_PRIMARY");
 }
-
-async function initScreenOrientation() {
-  await PORTRAIT_PRIMARY()
-}
-
-export const initValue = initScreenOrientation()
