0d5d3fa51756f7719432289e91f605302a4b23ba
zdu <zdu.strong@gmail.com>
2023-07-24 14:14:44 +0800

    #: Optimize code

--- react-cypress/cypress/page/Chat.ts
+++ react-cypress/cypress/page/Chat.ts
@@ -1,6 +1,6 @@
 export default {
   signOutButton: () => cy.xpath("//Button[.='Sign out']", { timeout: 60000 }),
   MessageContentInput: () => cy.xpath(`//fieldset[contains(., 'Message content')]/../textarea[@rows]`),
-  Message: (message: string) => cy.xpath(`//div[contains(@style, 'break-word')][contains(., '${message}')]`, { timeout: 30000 }),
-  RecallMessageButton: (message: string) => cy.xpath(`//div[contains(@style, 'break-word')][contains(., '${message}')]/..//button[contains(., 'Withdrawn')]`, { timeout: 30000 }),
+  Message: (message: string) => cy.xpath(`//div[contains(@style, 'break-word')][contains(., '${message}')]`, { timeout: 60000 }),
+  RecallMessageButton: (message: string) => cy.xpath(`//div[contains(@style, 'break-word')][contains(., '${message}')]/..//button[contains(., 'Withdrawn')]`, { timeout: 60000 }),
 }
