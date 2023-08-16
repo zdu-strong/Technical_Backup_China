6bad8fd584308800fddac405d7abbae42feb000d
zdu <zdu.strong@gmail.com>
2023-08-16 17:44:26 +0800

    Regenerate the changelog

--- springboot/src/main/resources/liquibase/changelog/2021.09.11/2021.09.11.04.00.00_changelog.sql
+++
@@ -1,6 +0,0 @@
--- liquibase formatted sql
--- changeset zdu:latest
-
-ALTER DATABASE CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE DATABASECHANGELOG  convert to CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE DATABASECHANGELOGLOCK  convert to CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- springboot/src/main/resources/liquibase/changelog/2023.09.06/2023.09.06.07.49.20_changelog.mysql.sql
+++
@@ -1,5 +0,0 @@
--- liquibase formatted sql
-
--- changeset John:1693986578199-1
-ALTER TABLE user_entity ADD password LONGTEXT NOT NULL;
-
