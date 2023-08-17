ba8e7af738479cf341202581dffb8798bd4bc44c
zdu <zdu.strong@gmail.com>
2023-08-17 22:49:41 +0800

    #: Fix e2e tests

--- react-cypress/cypress/page/Chat.ts
+++ react-cypress/cypress/page/Chat.ts
@@ -1,5 +1,5 @@
 export default {
-  signOutButton: () => cy.xpath("//Button[.='Sign out']", { timeout: 60000 }),
+  signOutButton: () => cy.xpath("//Button[.='Sign out']", { timeout: 180000 }),
   MessageContentInput: () => cy.xpath(`//fieldset[contains(., 'Message content')]/../textarea[@rows]`),
   Message: (message: string) => cy.xpath(`//div[contains(@style, 'break-word')][contains(., '${message}')]`, { timeout: 180000 }),
   RecallMessageButton: (message: string) => cy.xpath(`//div[contains(@style, 'break-word')][contains(., '${message}')]/..//button[contains(., 'Withdrawn')]`, { timeout: 60000 }),
