e34aade98ee6dd60c9aa924adf3c5fd9c3d80c03
zdu <zdu.strong@gmail.com>
2023-08-18 08:57:23 +0800

    #: Check if timeZone is valid

--- springboot/src/main/java/com/springboot/project/common/TimeZoneUtil/TimeZoneUtils.java
+++ springboot/src/main/java/com/springboot/project/common/TimeZoneUtil/TimeZoneUtils.java
@@ -3,6 +3,8 @@ package com.springboot.project.common.TimeZoneUtil;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.regex.Pattern;
+
 import org.springframework.stereotype.Component;
 
 @Component
@@ -19,6 +21,14 @@ public class TimeZoneUtils {
         ZonedDateTime zonedDateTime = Instant.now().atZone(zoneId);
         timeZone = String.format("%tz", zonedDateTime);
         timeZone = timeZone.substring(0, 3) + ":" + timeZone.substring(3, 5);
+        if (timeZone.length() != 6) {
+            throw new RuntimeException("invalid time zone");
+        }
+        if (!Pattern.compile(
+                "^[" + Pattern.quote("+") + Pattern.quote("-") + "]{1}" + "[0-9]{2}" + Pattern.quote(":") + "[0-9]{2}$")
+                .matcher(timeZone).find()) {
+            throw new RuntimeException("invalid time zone");
+        }
         return timeZone;
     }
 
