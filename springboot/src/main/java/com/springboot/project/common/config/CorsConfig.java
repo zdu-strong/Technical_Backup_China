6b2933f8ce5bba4de7e5bebcf76ff3059a1c4711
zdu <zdu.strong@gmail.com>
2023-09-15 19:40:18 +0800

    #: Modify cross-domain configuration

--- springboot/src/main/java/com/springboot/project/common/config/CorsConfig.java
+++ springboot/src/main/java/com/springboot/project/common/config/CorsConfig.java
@@ -1,6 +1,7 @@
 package com.springboot.project.common.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,7 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
+        registry.addMapping("/**").allowedOrigins(CorsConfiguration.ALL).allowedMethods(CorsConfiguration.ALL)
+                .allowedHeaders(CorsConfiguration.ALL).allowCredentials(true);
     }
 
 }
