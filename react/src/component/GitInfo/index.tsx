611a1ababdc938d2d330197b0b10855add84615e
zdu <zdu.strong@gmail.com>
2023-10-12 14:32:10 +0800

    example

--- react/src/component/GitInfo/index.tsx
+++ react/src/component/GitInfo/index.tsx
@@ -1,77 +1,49 @@
 import { observer, useMobxState } from "mobx-react-use-autorun";
-import GitInfo from 'react-git-info/macro'
-import { useMount } from "mobx-react-use-autorun";
-import { format, parseJSON } from "date-fns";
-import api from "@/api";
-import { GitPropertiesModel } from "@/model/GitPropertiesModel";
-import { FormattedMessage } from "react-intl";
-import LoadingOrErrorComponent from "@/common/LoadingOrErrorComponent/LoadingOrErrorComponent";
+import { stylesheet } from "typestyle";
+import { TextField } from "@mui/material";
+
+const css = stylesheet({
+  container: {
+    width: "20em",
+    $nest: {
+      "& > div:first-child": {
+        paddingTop: "3em",
+        paddingLeft: "1em",
+        paddingRight: "1em",
+        paddingBottom: "1em",
+        display: "flex",
+        justifyContent: "right"
+      },
+      "& > div:last-child": {
+        paddingLeft: "1em",
+        paddingRight: "1em",
+        textAlign: "right"
+      }
+    }
+  }
+});
 
 export default observer(() => {
 
   const state = useMobxState({
-    clientGitInfo: GitInfo(),
-    serverGitInfo: null as any as GitPropertiesModel,
-    ready: false,
-    error: null as any,
+    name: '',
   })
 
-  useMount(async () => {
-    try {
-      await loadServerGitInfo();
-      state.ready = true;
-    } catch {
-      state.error = new Error("Network Error");
-    }
-  })
-
-  async function loadServerGitInfo() {
-    const { data } = await api.Git.getServerGitInfo();
-    state.serverGitInfo = data;
-  }
-
-  return <LoadingOrErrorComponent ready={state.ready} error={state.error}>
-    {
-      state.ready && <>
-        <div className="w-full h-full flex justify-center flex-col items-center">
-          <div className="flex flex-row">
-            <div className="flex flex-row">
-              <FormattedMessage id="FrontEndCommitId" defaultMessage="Front-end commit id" />
-              {`: `}
-            </div>
-            <div className="flex flex-row" id="FrontEndCommitIdInfo" style={{ marginLeft: "1em" }}>
-              {state.clientGitInfo.commit.hash}
-            </div>
-          </div>
-          <div className="flex flex-row">
-            <div className="flex flex-row">
-              <FormattedMessage id="BackendCommitId" defaultMessage="Backend commit id" />
-              {`: `}
-            </div>
-            <div className="flex flex-row" id="BackendCommitIdInfo" style={{ marginLeft: "1em" }}>
-              {state.serverGitInfo.commitId}
-            </div>
-          </div>
-          <div className="flex flex-row">
-            <div className="flex flex-row">
-              <FormattedMessage id="FrontEndUpdateTime" defaultMessage="Front-end update time" />
-              {`: `}
-            </div>
-            <div className="flex flex-row" id="FrondEndUpdateDateInfo" style={{ marginLeft: "1em" }}>
-              {format(parseJSON(state.clientGitInfo.commit.date), "yyyy-MM-dd HH:mm")}
-            </div>
-          </div>
-          <div className="flex flex-row">
-            <div className="flex flex-row">
-              <FormattedMessage id="BackendUpdateTime" defaultMessage="Backend update time" />
-              {`: `}
-            </div>
-            <div className="flex flex-row" id="BackendUpdateDateInfo" style={{ marginLeft: "1em" }}>
-              {format(state.serverGitInfo.commitDate, "yyyy-MM-dd HH:mm")}
-            </div>
-          </div>
-        </div>
-      </>
-    }
-  </LoadingOrErrorComponent>
+  return <div className={css.container}>
+    <div>
+      <TextField
+        label={"Your Name"}
+        variant="outlined"
+        onChange={(e) => {
+          state.name = e.target.value;
+        }}
+        value={state.name}
+        autoComplete="off"
+        autoFocus={true}
+      />
+    </div>
+    <div>
+      {`- Welcome back, ${state.name}!`}
+    </div>
+  </div>
 })
