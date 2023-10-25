dc1d926dae9b1b5a94234310520713179878f2f4
zdu <zdu.strong@gmail.com>
2023-10-25 19:12:31 +0800

    #: Optimize code

--- ffcreator/.eslintrc.js
+++ ffcreator/.eslintrc.js
@@ -21,6 +21,6 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': 'warn',
-    'prefer-const': 'warn'
+    'prefer-const': 'warn',
   },
 };
