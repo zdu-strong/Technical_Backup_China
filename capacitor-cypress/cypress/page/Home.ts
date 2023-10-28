ca8146a36c43ac74905c81ee042da8e088d898ab
zdu <zdu.strong@gmail.com>
2023-10-28 17:07:48 +0800

    #: Fix unit tests

--- capacitor-cypress/cypress/page/Home.ts
+++ capacitor-cypress/cypress/page/Home.ts
@@ -2,6 +2,6 @@ export default {
   CurrentPower: () => cy.xpath(`//*[contains(text(),'current battery')]`),
   EnterGame: () => cy.xpath(`//button[contains(., 'Enter the game')]`),
   Game: () => cy.xpath(`//canvas`),
-  Setting: () => cy.xpath(`//*[@data-testid='SettingsIcon']/..`),
+  Setting: () => cy.xpath(`//*[@data-icon='gear']/..`),
   EndTheGameButton: () => cy.xpath(`//button[contains(., 'End Game')]`),
 }
